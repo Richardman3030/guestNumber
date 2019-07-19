ab555f6b088d71729acfceb1371516b25261e2d8
Richard Chien <richardman3030@gmail.com>
2019-07-19 15:45:12 +0800

    Initial Commit

---
+++ guestNumber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  guestNumber
+//
+//  Created by 簡立維 on 2019/7/19.
+//  Copyright © 2019 Kyowa Kirin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
